27f9f2be20ec6f49b541c7b78a1549dfa7ef154e
Max Gomes <maxgomes92@gmail.com>
2022-06-10 17:05:48 +0200

    Class 23

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -18,7 +22,26 @@ public class UserResource {
     }
 
     @GetMapping(path = "users/{id}")
-    public User retrieveUser(@PathVariable int id) {
-        return userDaoService.findOne(id);
+    public ResponseEntity<Object> retrieveUser(@PathVariable int id) {
+        var user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("User id " + id + " was not found.");
+        }
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping(path = "users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        var saved = userDaoService.save(user);
+
+        var location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saved.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
